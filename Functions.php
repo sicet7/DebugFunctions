52c7be8a4b9ffa54bc103b0943854b50539bd9de
Martin René Sørensen <sicet7@hotmail.com>
2015-06-03 22:44:28 +0200

    Created Functions file and updated ".gitignore" file.

---
+++ Functions.php
@@ -0,0 +1,5 @@
+<?php
+//created by Martin René Sørensen aka. Sicet7
+// Functions.php - 22:43 2015/06/03
+//for DebugFunctions
+
---
+++ test/index.php
@@ -0,0 +1,5 @@
+<?php
+//created by Martin René Sørensen aka. Sicet7
+// index.php - 22:42 2015/06/03
+//for DebugFunctions
+

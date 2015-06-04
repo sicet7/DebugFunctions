75bc2031909d225d533789f1d1fb25d774d407f6
Martin René Sørensen <sicet7@hotmail.com>
2015-06-04 19:22:56 +0200

    Created Debug Functions "dvd" and "vds".

--- Functions.php
+++ Functions.php
@@ -3,3 +3,18 @@
 // Functions.php - 22:43 2015/06/03
 //for DebugFunctions
 
+function vds($item){//var dumb string
+    ob_start();
+    var_dump($item);
+    $result = ob_get_clean();
+    return $result;
+}
+
+function dvd(){//die var dumb
+    $args = func_get_args();
+    $string = '';
+    foreach($args as $variable){
+        $string .= vds($variable);
+    }
+    die('<pre>'.$string);
+}
